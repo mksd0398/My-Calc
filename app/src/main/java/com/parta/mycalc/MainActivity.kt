d727322679fe2230af6c07ce3b4a4f50c67654d5
mksd0398 <mayank0398@gmail.com>
2022-04-21 17:34:50 +0530

    accessing buttons and input without creating objects in class

--- app/src/main/java/com/parta/mycalc/MainActivity.kt
+++ app/src/main/java/com/parta/mycalc/MainActivity.kt
@@ -4,9 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import android.view.WindowManager
-import android.widget.Button
-import android.widget.EditText
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
@@ -16,92 +14,49 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     // Represent that current state is in error or not
     var stateError: Boolean = false
     // If true, do not allow to add another DOT
-    var lastDot: Boolean = false
-
-    //objects
-    private lateinit var btn1: Button
-    private lateinit var btn2: Button
-    private lateinit var btn3: Button
-    private lateinit var btn4: Button
-    private lateinit var btn5: Button
-    private lateinit var btn6: Button
-    private lateinit var btn7: Button
-    private lateinit var btn8: Button
-    private lateinit var btn9: Button
-    private lateinit var btnZero: Button
-    private lateinit var btnAdd: Button
-    private lateinit var btnSub: Button
-    private lateinit var btnTimes: Button
-    private lateinit var btnDiv: Button
-    private lateinit var btnEquals: Button
-    private lateinit var btnDot: Button
-    private lateinit var btnClear: Button
-    private lateinit var btnOpenBrackets: Button
-    private lateinit var btnCloseBrackets: Button
-    private lateinit var input: EditText
-
-
+    private var lastDot: Boolean = false
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btn1= findViewById(R.id.btn_1)
+        // beauty of kotlin
+        // Initialize Buttons without creating objects
         btn1.setOnClickListener(this)
-        btn2 = findViewById(R.id.btn_2)
         btn2.setOnClickListener(this)
-        btn3 = findViewById(R.id.btn_3)
         btn3.setOnClickListener(this)
-        btn4 = findViewById(R.id.btn_4)
         btn4.setOnClickListener(this)
-        btn5 = findViewById(R.id.btn_5)
         btn5.setOnClickListener(this)
-        btn6 = findViewById(R.id.btn_6)
         btn6.setOnClickListener(this)
-        btn7 = findViewById(R.id.btn_7)
         btn7.setOnClickListener(this)
-        btn8 = findViewById(R.id.btn_8)
         btn8.setOnClickListener(this)
-        btn9 = findViewById(R.id.btn_9)
         btn9.setOnClickListener(this)
-        btnZero = findViewById(R.id.btn_zero)
         btnZero.setOnClickListener(this)
-        btnDot = findViewById(R.id.btn_dot)
         btnDot.setOnClickListener(this)
-        btnEquals = findViewById(R.id.btn_equals)
         btnEquals.setOnClickListener(this)
-        btnDiv  = findViewById(R.id.btn_div)
         btnDiv.setOnClickListener(this)
-        btnAdd = findViewById(R.id.btn_add)
         btnAdd.setOnClickListener(this)
-        btnSub  = findViewById(R.id.btn_sub)
         btnSub.setOnClickListener(this)
-        btnTimes  = findViewById(R.id.btn_times)
         btnTimes.setOnClickListener(this)
-        btnClear = findViewById(R.id.btn_clear)
         btnClear.setOnClickListener(this)
-        btnOpenBrackets = findViewById(R.id.btn_open_bracket)
-        btnOpenBrackets.setOnClickListener(this)
-        btnCloseBrackets = findViewById(R.id.btn_close_bracket)
-        btnCloseBrackets.setOnClickListener(this)
-
-        input = findViewById(R.id.input)
+        btnOpenBracket.setOnClickListener(this)
+        btnCloseBracket.setOnClickListener(this)
 
     }
 
     override fun onClick(p0: View?) {
         when(p0){
-            btnOpenBrackets ->{
+            btnOpenBracket ->{
                 input.append("(")
             }
 
-            btnCloseBrackets ->{
+            btnCloseBracket ->{
                 input.append(")")
             }
             btnSub -> {
                 if (lastNumeric && !stateError) {
-                    input.append("-")
+                    input.append("-") // accessing input without object
                     lastNumeric = false
                     lastDot = false
                 }
@@ -186,7 +141,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                         lastDot = true // Result contains a dot
                     } catch (ex: ArithmeticException) {
                         // Display an error message
-                        input.setText( "Error")
+                        input.setText(getString(R.string.error))
                         stateError = true
                         lastNumeric = false
                     }
@@ -195,7 +150,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         }
     }
 
-    private fun onDigit(str:String){
+    private fun onDigit(str: String){
         if (stateError) {
             input.setText(str)
             stateError = false
